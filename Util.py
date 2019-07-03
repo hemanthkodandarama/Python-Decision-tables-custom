392cb5e6b4f3d08825650fe548427961dc9711e2
repl-hemanth-kumar <hemanth.k@replicon.com>
2019-07-03 19:46:27 +0530

    code refactor for print data

--- Util.py
+++ Util.py
@@ -1,3 +1,25 @@
 def Between(number,fromnumber,tonumber):
     #return number > fromnumber and number<=tonumber
     return number > fromnumber
+
+def GetPayDetails(timeentry,paycodes):
+    data = ','
+    for paycode in paycodes:
+        data += ' %s = %s ,' %( paycode ,(str(timeentry[paycode])  if timeentry.has_key(paycode) else '0'))
+    return data;
+
+def MapActionPayData(test_timeEntries,paycodes):
+    printData = ''
+    for timeentry in test_timeEntries:
+        for action in timeentry['actions']:
+            if not (action.has_key('MetaData') and action.has_key('PayHours') \
+                            and action.has_key('PayCode') and action['PayHours'] >0):
+                    continue
+            if not timeentry.has_key(action['PayCode']):
+                timeentry[action['PayCode']]=0
+
+            timeentry[action['PayCode']] += action['PayHours'] 
+
+        printData+= '\n %s - %s - %s  ==  %s ' % ( timeentry['DayNumber'],timeentry['Date'],timeentry['TimeEntryHours'] , GetPayDetails(timeentry,paycodes))
+
+    return printData
--- pydt_CaliforniaTest.py
+++ pydt_CaliforniaTest.py
@@ -1,5 +1,6 @@
 import pydt
 import numpy as np
+import Util
 
 decisiontbl = pydt.load_xls("California-DecisionTable.xls")
 ConditionNames = pydt.GetUniqueConditionsNames(decisiontbl)
@@ -12,19 +13,5 @@ paycodes = map(lambda x: x,np.unique(map(lambda x: x[5] , decisiontbl["data"])))
 for entry in test_timeEntries:
     pydt.ProcessTimeEntry(ConditionNames,test_timeEntries,entry,decisiontbl)
 
-def GetPayDetails(timeentry):
-    data = ','
-    for paycode in paycodes:
-        data += ' %s = %s ,' %( paycode ,(str(timeentry[paycode])  if timeentry.has_key(paycode) else '0'))
-    return data;
-
-for timeentry in test_timeEntries:
-    for action in timeentry['actions']:
-        if not (action.has_key('MetaData') and action.has_key('PayHours') \
-                        and action.has_key('PayCode') and action['PayHours'] >0):
-                continue
-        if not timeentry.has_key(action['PayCode']):
-            timeentry[action['PayCode']]=0
-
-        timeentry[action['PayCode']] += action['PayHours'] 
-    print(' %s - %s - %s  ==  %s ' % ( timeentry['DayNumber'],timeentry['Date'],timeentry['TimeEntryHours'] , GetPayDetails(timeentry)))
+printdata = Util.MapActionPayData(test_timeEntries,paycodes)
+print(printdata)
